8903754b986d8045050a73c6676c039823dccb17
Samuel Brito <sambriton7@gmail.com>
2023-12-11 10:41:09 -0300

    Creating a car schema

---
+++ prisma/migrations/20231211132917_init/migration.sql
@@ -0,0 +1,24 @@
+/*
+  Warnings:
+
+  - Added the required column `link` to the `Car` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Car" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "model" TEXT NOT NULL,
+    "doors" INTEGER NOT NULL,
+    "color" TEXT NOT NULL,
+    "type" TEXT NOT NULL,
+    "carChange" TEXT NOT NULL,
+    "hasAir" BOOLEAN NOT NULL,
+    "link" TEXT NOT NULL
+);
+INSERT INTO "new_Car" ("carChange", "color", "doors", "hasAir", "id", "model", "name", "type") SELECT "carChange", "color", "doors", "hasAir", "id", "model", "name", "type" FROM "Car";
+DROP TABLE "Car";
+ALTER TABLE "new_Car" RENAME TO "Car";
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
--- src/controllers/Car/carSchema.ts
+++ src/controllers/Car/carSchema.ts
@@ -1,3 +1,19 @@
 import {z} from 'zod'
 import { buildJsonSchemas } from 'fastify-zod'
 
+const carCore = z.object({
+    name: z.string(),
+    model: z.string(),
+    doors: z.number(),
+    color: z.string(),
+    type: z.string(),
+    carChange: z.string(),
+    hasAir: z.boolean(),
+    link: z.string()
+})
+
+export type carCore = z.infer<typeof carCore>
+
+export const {schemas: carSchema, $ref} = buildJsonSchemas({
+    carCore
+})
