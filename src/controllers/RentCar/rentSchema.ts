ebcdfd9353618c821611b98d5c5e259334efd1c2
Samuel Brito <sambriton7@gmail.com>
2023-12-15 11:08:41 -0300

    Creating Rent Schema

---
+++ src/controllers/RentCar/rentSchema.ts
@@ -0,0 +1,26 @@
+import { buildJsonSchemas } from 'fastify-zod'
+import {z} from 'zod'
+
+const rentCore = {
+    locatedAt: z.date(),
+    devolutionTime: z.date(),
+    userId: z.number(),
+    carId: z.number()
+}
+
+const createRent = z.object({
+    ...rentCore
+})
+
+const responseRent = z.object({
+    id: z.number(),
+    ...rentCore
+})
+
+export type createRentSchema = z.infer<typeof createRent>
+export type responseRentSchema = z.infer<typeof responseRent>
+
+export const {schemas: rentSchemas, $ref} = buildJsonSchemas({
+    createRent,
+    responseRent
+})
