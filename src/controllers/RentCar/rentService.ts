311c0ac1513082978d2eef1da5c7d1ced2731f35
your_github_username <your_github_email>
2023-12-31 16:52:06 -0300

    Creating a rent function in rentService

--- src/controllers/RentCar/rentService.ts
+++ src/controllers/RentCar/rentService.ts
@@ -3,45 +3,71 @@ import { createRentSchema } from "./rentSchema"
 
 const today = new Date("December 17, 1995 03:24:00")
 
-export async function rentAvailableCars() {
-    const rents = prisma.rent.findMany({
-        select: {
-                id: true,
-                locatedAt: true,
-                devolutionTime: true,
-                userId: true,
-                carId: true
-                },
+export async function getAvailableCarsInInterval(since: Date, until: Date) {
+    const cars = prisma.car.findMany({
+        
         where: {
+            rent: {
                 NOT: {
-                    locatedAt: {
-                        lte: today
-                    },
-                    devolutionTime: {
-                        gte: today
-                    }
+                    AND: [
+                        {
+                            locatedAt: {
+                                lte: since
+                            },
+                        },
+                        {
+                            devolutionTime: {
+                                gte: until
+                            }
+                        }
+                    ]
                 }
+            }
         }
     })
 
-    return rents
+    return cars
 }
 
 export async function createRent(params: createRentSchema) {
     
-    const {userId, ...rentData} = params
+    const {userId,carId, ...rentData} = params
 
-    const user = await prisma.user.findUnique({
+    const userFound = await prisma.user.findUnique({
         where: {
             id: userId
         }
     })
+
+    if(!userFound) {
+        throw new Error('User not found')
+    }
+
+    const findCar = await prisma.car.findUnique({
+        where: {
+            id: carId
+        }
+    })
+
+    if(!findCar) {
+        throw new Error('Car not found')
+    }
     
     const newRent = await prisma.rent.create({
         data: {
             ...rentData,
-            userId: user!.id
+            user: {
+                connect: {
+                    id: userId
+                }
+            },
+            rentedCar: {
+                connect: {
+                    id: carId
+                }
+            }
         }
     })
+
     return newRent
 }
