f761c9bd1cc1d28fb6009b8eac7aca9fba15f184
Samuel-Brito19 <sambriton7@gmail.com>
2024-03-06 20:22:49 -0300

    Vercel file

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,6 @@ async function main() {
         const PORT = process.env.PORT || 3000
         await fastify.listen({ port: Number(PORT) })
 
-        console.log('Server ready at http://localhost:3000')
     } catch (error) {
         console.log(error)
         process.exit(1)
